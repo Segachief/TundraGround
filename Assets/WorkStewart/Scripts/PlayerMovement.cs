1bcd6600394d3be73ca46dfe2b2f8668f1bfa4b5
Stewart Melville <segachief23@gmail.com>
2026-03-16 00:10:35 +0000

    Climbing
    Added climbing behaviour with a test ladder (can be used for trees as per mechanic outlined in GDC but a craftable ladder could be a good option for the crafting menu).

--- Assets/WorkStewart/Scripts/PlayerMovement.cs
+++ Assets/WorkStewart/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Callbacks;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,10 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 4f;
     [SerializeField] float jumpSpeed = 11f;
+    [SerializeField] float climbSpeed = 4;
+
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator myAnimator;
     CapsuleCollider2D myCapsuleCollider;
+    float startingGravity;
 
 
     void Start()
@@ -17,12 +21,14 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        startingGravity = rb.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -35,7 +41,8 @@ public class PlayerMovement : MonoBehaviour
         // Checks if the player is touching the Ground layer
         // If not, prevents the use of Jump
         if(value.isPressed && 
-            myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))
+            && myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             rb.linearVelocity += new Vector2(0f, jumpSpeed);
         }
@@ -47,14 +54,14 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = playerVelocity;
 
         bool hasHorizontalSpeed = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
-         if(hasHorizontalSpeed)
-         {
-             myAnimator.SetBool("isRunning", true);
-         }
-         else
-         {
-             myAnimator.SetBool("isRunning", false);
-         }
+        if(hasHorizontalSpeed)
+        {
+            myAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
     }
 
     void FlipSprite()
@@ -68,6 +75,29 @@ public class PlayerMovement : MonoBehaviour
         {
             // Flips the sprite based on its velocity x movement
             transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if(myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            // Prevents slide of player from gravity during climb
+            // If we want the player to gradually slide down a tree, then this
+            // can be removed or a check for tree/surface made to distinguish
+            rb.gravityScale = 0f;
+            Vector2 climbVelocity = new Vector2(rb.linearVelocity.x, moveInput.y * climbSpeed);
+            rb.linearVelocity = climbVelocity;
+
+            // May be good to add an idle anim for being on a climbable surface
+            // but not moving up or down; uses regular standing idle currently
+            bool hasVerticalSpeed = Mathf.Abs(rb.linearVelocity.y) > Mathf.Epsilon;
+            myAnimator.SetBool("isClimbing", hasVerticalSpeed); 
+        }
+        else
+        {
+            rb.gravityScale = startingGravity;
+            myAnimator.SetBool("isClimbing", false);
         }
     }
 }
