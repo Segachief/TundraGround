3ca297a9da9a544b24937af5ad3c77f958a77649
Michael <ovenbob58@gmail.com>
2026-05-10 15:43:43 +0100

    Bear AI fixes
    Added some better feature for the bear

---
+++ Assets/WorkMichael/Scripts/BearAI.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+
+public class BearPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform leftEdge; // Patrol Point
+    [SerializeField] private Transform rightEdge; // Patrol Point
+    [SerializeField] private Transform enemy; // Enemy
+    [SerializeField] private float speed; // Speed of Enemy 
+    private Vector3 initScale;
+    private bool movingLeft;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float idleDuration;
+    [SerializeField] private Transform player;
+    [SerializeField] private float chaseRange;
+    private bool chasing;
+    private float idleTimer;
+    private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Awake()
+    {
+        rb = enemy.GetComponent<Rigidbody2D>();
+        initScale = enemy.localScale;
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = movingLeft ? Vector2.left : Vector2.right;
+        Vector2 origin = groundCheck.position + (Vector3)(direction * 0.5f);
+        Debug.DrawRay(origin, Vector2.down * groundCheckDistance, Color.red);
+        bool groundAhead = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance, groundLayer);
+        float distanceToPlayer = Vector2.Distance(enemy.position, player.position);
+        float heightDifference = Mathf.Abs(player.position.y - enemy.position.y);
+        if (distanceToPlayer < chaseRange && heightDifference < 1f)
+        {
+            chasing = true;
+        }
+        else
+        {
+            chasing = false;
+        }
+
+        if (chasing)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            if (movingLeft)
+            {
+                if (enemy.position.x <= leftEdge.position.x)
+                {
+                    StopAndTurn();
+                    return;
+                }
+                if (!groundAhead)
+                {
+                    StopAndTurn();
+                    return;
+                }
+                MoveInDirection(-1);
+            }
+            else
+            {
+                if (enemy.position.x >= rightEdge.position.x)
+                {
+                    StopAndTurn();
+                    return;
+                }
+                if (!groundAhead)
+                {
+                    StopAndTurn();
+                    return;
+                }
+                MoveInDirection(1);
+            }
+        }
+    }
+
+    private void DirectionChange()
+    {
+        anim.SetBool("isWalking", false );
+        idleTimer += Time.deltaTime;
+        if (idleTimer > idleDuration)
+        {
+            movingLeft = !movingLeft;
+            idleTimer = 0;
+        }
+    }
+
+    private void StopAndTurn()
+    {
+        rb.linearVelocity = Vector2.zero;
+        DirectionChange();
+    }
+
+    private void MoveInDirection(int  _direction)
+{
+        idleTimer = 0;
+    anim.SetBool("isWalking", true );
+    //Make enemy face direction
+    enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * -_direction, initScale.y, initScale.z);
+
+        // Move in that direction
+        rb.linearVelocity = new Vector2(_direction * speed, rb.linearVelocity.y);
+    }
+
+    private void ChasePlayer()
+    {
+        anim.SetBool("isWalking", true);
+        //Face the player
+        if (player.position.x > enemy.position.x)
+            enemy.localScale = new Vector3(-Mathf.Abs(initScale.x), initScale.y, initScale.z);
+        else
+            enemy.localScale = new Vector3(Mathf.Abs(initScale.x), initScale.y, initScale.z);
+        float direction = Mathf.Sign(player.position.x - enemy.position.x);
+        rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+    }
+}
+  
