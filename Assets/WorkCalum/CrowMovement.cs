3ab0e6e283fa2c7092f95d3fe0404e33aa8414a6
CalumIY <calumiy77@gmail.com>
2026-05-12 15:53:31 +0100

    crow update

--- Assets/WorkCalum/CrowMovement.cs
+++ Assets/WorkCalum/CrowMovement.cs
@@ -8,6 +8,7 @@ public class CrowMovement : MonoBehaviour
     public bool chase = false;
     public bool squawking = false;
     public Transform startingPoint;
+    private CircleCollider2D circleCollider;
 
     private GameObject player;
 
@@ -29,23 +30,30 @@ public class CrowMovement : MonoBehaviour
         Flip();
     }
 
+    //controls crow chase mechanics
     private void Chase()
     {
+        //crow will target player and move towards them
         transform.position=Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+
+        // if crow is near player, chase is paused and squawk is activated
         if (Vector2.Distance(transform.position, player.transform.position) <= 2f)
         {
             speed = 0;
             squawk();
         }
+        // when player leaves crows stopping radius speed is reset and squawk is cancelled
         else
         {
-            speed = 1;
+            speed = 1.5f;
             squawking = false;
         }
     }
 
+    //controls crow return mechanics
     private void ReturnStartPoint()
     {
+        //when player leaves crows radius, crow will target is starting point and will move towards it
         transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
         if (transform.position.x > startingPoint.transform.position.x)
             transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -53,6 +61,7 @@ public class CrowMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 180, 0);
     }
 
+    //controls sprite direction
     private void Flip()
     {
         if (chase == true)
@@ -64,8 +73,11 @@ public class CrowMovement : MonoBehaviour
         }
     }
 
+    //controls player detection
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        //if player collides with crows radius chase = true
         if (collision.CompareTag("Player"))
         {
             chase = true;
@@ -74,19 +86,29 @@ public class CrowMovement : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //when player leaves crows radius chase = false
         if (collision.CompareTag("Player"))
         {
             chase = false;
         }
     }
 
+    //controls crow enemy control mechanics
     private void squawk()
     {
         squawking = true;
         if (squawking == true)
         {
             //atttract nearby enemies.
+            //activateCollider();
         }
 
+    }
+
+    private void activateCollider()
+    {
+        circleCollider = gameObject.AddComponent<CircleCollider2D>();
+        circleCollider.radius = 30f;
+        circleCollider.enabled = true;
     }
 }
