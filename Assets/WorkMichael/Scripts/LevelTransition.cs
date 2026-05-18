701e62b519952a9a3e417d7e8d455088952b1d66
Michael <ovenbob58@gmail.com>
2026-05-18 15:27:23 +0100

    i added properly commented code
    i commented my code

--- Assets/WorkMichael/Scripts/LevelTransition.cs
+++ Assets/WorkMichael/Scripts/LevelTransition.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // Script by Michael Arthur 
+using UnityEngine.SceneManagement; 
 
-public class LevelTransition : MonoBehaviour
+public class LevelTransition : MonoBehaviour // Script by Michael Arthur 
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -10,11 +10,11 @@ public class LevelTransition : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) // This method is called when a collision occurs with the Player object
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player") // Check if the colliding object is the player
         {
-            SceneManager.LoadScene("Level3");
+            SceneManager.LoadScene("Level3"); // Load the next scene named Level 3
         }
     }
 }
--- Assets/WorkMichael/Scripts/PlayerHide.cs
+++ Assets/WorkMichael/Scripts/PlayerHide.cs
@@ -1,30 +1,30 @@
 using UnityEngine;
 
-public class PlayerHide : MonoBehaviour
+public class PlayerHide : MonoBehaviour // Script by Michael Arthur
 {
-    private bool canHide = false;
-    private bool isHiding = false;
-    private SpriteRenderer spriteRenderer;
-    private Collider2D playerCollider;
+    private bool canHide = false; // Indicates if the player is in a hideable area
+    private bool isHiding = false; // Indicates if the player is currently hiding
+    private SpriteRenderer spriteRenderer; // Reference to the player's SpriteRenderer
+    private Collider2D playerCollider; // Reference to the player's Collider2D
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        playerCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
+        playerCollider = GetComponent<Collider2D>(); // Get the Collider2D component
     }
 
     void Update()
     {
-        if (canHide && Input.GetKeyDown(KeyCode.W))
+        if (canHide && Input.GetKeyDown(KeyCode.W)) // Check if the player can hide and if the 'W' key is pressed
         {
-            ToggleHide();
+            ToggleHide(); // Toggle the hiding state
         }
     }
 
     private void ToggleHide()
-    {
-        isHiding = !isHiding;
-        if (isHiding)
+    { // I had some help with ChatGPT with making the player semi transparent as i wasn't sure how to do that
+        isHiding = !isHiding; // Toggle the hiding state
+        if (isHiding) // If the player is now hiding make them semi transparent and ignore collisions with enemies
         {
             spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
             IgnoreEnemies(true);
@@ -38,39 +38,39 @@ public class PlayerHide : MonoBehaviour
 
     private void IgnoreEnemies(bool ignore)
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        foreach (GameObject enemy in enemies)
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Find all enemies in the scene
+        foreach (GameObject enemy in enemies) // Loop through each enemy and ignore or re enable collisions based on the hiding state
         {
-            Collider2D enemyCollider = enemy.GetComponent<Collider2D>();
-            if (enemyCollider != null)
+            Collider2D enemyCollider = enemy.GetComponent<Collider2D>(); // Get the Collider2D component of the enemy
+            if (enemyCollider != null) // Check if the enemy has a Collider2D component
             {
-                Physics2D.IgnoreCollision(playerCollider, enemyCollider, ignore);
+                Physics2D.IgnoreCollision(playerCollider, enemyCollider, ignore); // Ignore or re enable collisions between the player and the enemy based on the hiding state
             }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // Check if the player enters a hideable area
     {
-        if (other.CompareTag("HideSpot"))
+        if (other.CompareTag("HideSpot")) // Check if the collider belongs to a hide spot
         {
-            canHide = true;
+            canHide = true; // Allow the player to hide
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other) // Check if the player exits a hideable area
     {
-        if (other.CompareTag("HideSpot"))
+        if (other.CompareTag("HideSpot")) // Check if the collider belongs to a hide spot
         {
-            canHide = false;
-            if (isHiding)
+            canHide = false; // Disallow the player to hide
+            if (isHiding) // If the player is currently hiding toggle it off when they exit the hide spot
             {
-                ToggleHide();
+                ToggleHide(); // Toggle the hiding state off
             }
         }
     }
 
-    public bool IsHiding()
+    public bool IsHiding() // Method to check if the player is currently hiding
     {
-        return isHiding;
+        return isHiding; // Return the current hiding state
     }
 }
