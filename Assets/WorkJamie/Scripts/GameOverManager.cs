f4157fb86bb5ac328cc5d6df65ab78090d292cb0
Stewart Melville <segachief23@gmail.com>
2026-05-19 11:50:55 +0100

    Game Over Update
    Updated to use level manager (and by extension audio manager) so that level music plays on restart

--- Assets/WorkJamie/Scripts/GameOverManager.cs
+++ Assets/WorkJamie/Scripts/GameOverManager.cs
@@ -4,35 +4,59 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour // This script was written by Jamie
+// and then updated by SM hehe
 {
-    
-   
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private LevelManager levelManager;
+
+    void Awake()
     {
-        
+        levelManager = FindFirstObjectByType<LevelManager>();
     }
 
     public void ButtonPressed(GameObject button)
     {
         if(button.name == "MainMenu")
         {
-            SceneManager.LoadScene("MainMenu");
+            levelManager.LoadMainMenu();
         }
         else if(button.name == "Retry")
         {   
-            //LastPlayedScene is set in PlayerHealth on Start() so Retry will always play the last level the player played.
-            SceneManager.LoadScene(PlayerPrefs.GetString("LastPlayedScene"));
-        }
+            // LastPlayedScene is set in PlayerHealth on Start() so Retry
+            // will always play the last level the player played. - Jamie
 
+            // Updated to use LevelManager - SM
+            SceneManager.LoadScene(PlayerPrefs.GetString("LastPlayedScene"));
+            switch(PlayerPrefs.GetString("LastPlayedScene"))
+            {
+                case "Level1":
+                levelManager.LoadLevel1();
+                break;
+
+                case "Level2":
+                levelManager.LoadLevel2();
+                break;
+
+                case "Level3":
+                levelManager.LoadLevel3();
+                break;
+
+                case "Level4":
+                levelManager.LoadLevel4();
+                break;
+
+                case "Level5":
+                levelManager.LoadLevel5();
+                break;
+
+                default:
+                levelManager.LoadMainMenu();
+                break;
+            }
+        }
         else
         {
-            Debug.LogWarning("Unkown button pressed on GameOver Scene");
+            Debug.LogWarning("Unknown button pressed on GameOver Scene");
         }
     }
 }
